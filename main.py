0bfb5fd2fb0e5078efe134cf788598283a1f086a
Eric Chan <eachan10@gmail.com>
2023-03-05 10:41:53 -0800

    Allow selection of maze file

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 from floodfill import floodfill
 from parse import parse
 
+import sys
 
 
 # mazes directory should only have maze files otherwise chaos ensues
@@ -30,12 +31,18 @@ def main(stdscr):
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_GREEN)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)
 
-    for maze in os.listdir("mazes"):
-        with open(f"mazes/{maze}") as f:
+    if len(sys.argv) > 1:
+        f_path = sys.argv[1]
+        mazes = [f_path]
+    else:
+        mazes = (f"mazes/{maze}" for maze in os.listdir("mazes"))
+
+    for maze in mazes:
+        with open(maze) as f:
             maze = parse(f.readlines())
         floodfill(maze, stdscr, DELAY)
         time.sleep(2)
-    stdscr.addstr(0, 67, "DONE")
+    stdscr.addstr(0, 67, "DONE: Press any key to continue...")
     stdscr.getch()
 
 
